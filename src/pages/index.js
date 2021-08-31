1a4c8fd3a2e66f1c61d14b1c3d672a6447014dc6
Ethan-Tsao <tsao.ethan33@gmail.com>
2021-08-30 23:02:06 -0500

    updated storyboards page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import { Layout } from "../components/Layout";
-import { Image, VStack, Center, Heading } from "@chakra-ui/react";
+import { Image, VStack, Center, Heading, Stack, Link } from "@chakra-ui/react";
 import Head from "next/head";
+import { useEffect } from "react";
 
 const Index = () => {
   return (
@@ -20,14 +21,42 @@ const Index = () => {
           >
             Storyboards
           </Heading>
-          <Image src="/CONTACTME.png" />
+          {/* Rabbit-Napped Speaker Deck */}
           <iframe
-            title="vimeo-player"
+            title="Rabbit-Napped-Excerpt"
+            src="https://speakerdeck.com/ncarmouche/rabbit-napped-storyboard-excerpt-natalie-carmouche"
+            width="100%"
+          />
+          <Heading size="md" textAlign="center">
+            If the Speaker Deck does not load, click{" "}
+            <Link
+              href="https://speakerdeck.com/ncarmouche/rabbit-napped-storyboard-excerpt-natalie-carmouche"
+              color="teal.500"
+            >
+              THIS LINK
+            </Link>
+          </Heading>
+          {/* Rabbit 1,2,3 Stack Row */}
+          <Stack maxw="80%" direction="row" justify="center">
+            <Image src="/rabbit1.png" boxSize="33%" objectFit="cover" />
+            <Image src="/rabbit2.png" boxSize="33%" objectFit="cover" />
+            <Image src="/rabbit3.png" boxSize="33%" objectFit="cover" />
+          </Stack>
+          {/* Script Thumbs */}
+          <Image src="/script_thumbs.png" objectFit="cover" />
+
+          {/* Sid and Mort Stack Row */}
+          <Stack direction="row" justify="center">
+            <Image src="/Sid.png" boxSize="50%" objectFit="cover" />
+            <Image src="/Mort.png" boxSize="50%" objectFit="cover" />
+          </Stack>
+
+          {/* Opinatus */}
+          <iframe
+            title="Opinatus"
             src="https://player.vimeo.com/video/310716029"
             width="640"
             height="360"
-            frameborder="0"
-            allowfullscreen
           />
         </VStack>
       </Center>
